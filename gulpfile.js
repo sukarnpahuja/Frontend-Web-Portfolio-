8dda67929a27f14f500ae3f520d074696413e1da
sukarnpahuja <43109006+sukarnpahuja@users.noreply.github.com>
2020-07-06 13:39:36 +0530

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const wait = require('gulp-wait');
+const babel = require('gulp-babel');;
+const rename = require('gulp-rename');
+
+gulp.task('scripts', function() {
+    return gulp.src('./js/scripts.js')
+        .pipe(plumber(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        })))
+        .pipe(babel({
+          presets: [['@babel/env', {modules:false}]]
+        }))
+        .pipe(uglify({
+            output: {
+                comments: '/^!/'
+            }
+        }))
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('./js'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src('./scss/styles.scss')
+        .pipe(wait(250))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./js/scripts.js', gulp.series('scripts'));
+    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
